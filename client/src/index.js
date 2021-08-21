dca56d06aad38388354da20938253cd6273d4d20
Geraldine <hello@geraldiner.com>
2021-08-20 21:13:19 -1000

    update query paths

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
-const url = (process.env.NODE_ENV = "development" ? "http://localhost:4000" : "https://geraldiner-com.herokuapp.com");
+console.log(process.NODE_ENV);
+
+const url = process.env.NODE_ENV !== "production" ? "http://localhost:4000" : "https://geraldiner-com.herokuapp.com";
 
 const client = new ApolloClient({
 	uri: url,
