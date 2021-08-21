362e9127494bbb48c2264df3e46b2c20216e2f85
Geraldine <hello@geraldiner.com>
2021-08-21 11:21:28 -1000

    add tailwind for styling

---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+	purge: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.jsx"],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [
+		require("@tailwindcss/forms")({
+			strategy: "class",
+		}),
+	],
+};
