dd4930669300751b8a65b34121d22b47ab7cae88
Geraldine <hello@geraldiner.com>
2021-08-22 21:32:45 -1000

    update netlify form

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -5,37 +5,8 @@ const Contact = () => {
 		<>
 			<div className="bg-white rounded-md p-3 mx-4 lg:mx-16">
 				<h3 className="text-2xl lg:text-3xl my-3">Let's chat!</h3>
-				<form name="contact" method="POST" data-netlify="true">
+				<form name="contact" method="post" className="space-y-5" data-netlify="true">
 					<input type="hidden" name="form-name" value="contact" />
-					<div>
-						<label htmlFor="name">Name</label>
-						<input id="name" type="text" name="name" />
-					</div>
-					<div>
-						<label>Email</label>
-						<input id="email" type="email" name="email" />
-					</div>
-					<div>
-						<label htmlFor="message">Message</label>
-						<textarea id="message" name="message"></textarea>
-					</div>
-					<button type="submit">Send Message</button>
-				</form>
-				{/* <form
-					name="contact"
-					method="post"
-					action="/thanks/"
-					className="space-y-5"
-					data-netlify-honeypot="bot-field"
-					data-netlify-recaptcha="true"
-					data-netlify="true"
-				>
-					<input type="hidden" name="form-name" value="contact" />
-					<div className="hidden">
-						<label className="block mb-1 font-bold text-gray-500" htmlFor="bot-field">
-							Don’t fill this out if you’re human: <input name="bot-field" />
-						</label>
-					</div>
 					<div>
 						<label htmlFor="name" className="block mb-1 font-bold text-gray-500">
 							Name
@@ -60,14 +31,13 @@ const Contact = () => {
 							rows="3"
 						></textarea>
 					</div>
-					<div data-netlify-recaptcha="true"></div>
 					<button
 						type="submit"
 						className="block w-full bg-blue hover:bg-yellow p-4 rounded text-white font-bold hover:text-gray-900 transition duration-300"
 					>
 						Send
 					</button>
-				</form> */}
+				</form>
 			</div>
 		</>
 	);
